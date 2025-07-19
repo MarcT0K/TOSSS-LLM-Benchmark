06b23861597c7600b4be1c5e80aed21ec2062a3a
DAMIE Marc <marc.damie@inria.fr>
2025-07-19 21:19:53 +0200

    Add script to download data

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
+## Install requirements
+sudo apt install python3-pip wget unzip
 pip3 install llm llm-mistral
+
+
+## Download dataset
+wget -O megavul.zip https://www.kaggle.com/api/v1/datasets/download/marcdamie/megavul-a-cc-java-vulnerability-dataset?datasetVersionNumber=1
+unzip megavul.zip
+rm megavul.zip
